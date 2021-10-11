8b327579a0f833f1e4814e8365df42c8180eb896
ignatandrei <ignatandrei@yahoo.com>
2021-10-11 09:01:21 +0300

    all projects

--- src/MyProgrammerAll/MyProgrammerVSProjects/FindProjects.cs
+++ src/MyProgrammerAll/MyProgrammerVSProjects/FindProjects.cs
@@ -49,8 +49,7 @@ namespace MyProgrammerVSProjects
                 var app =Analyze(item);
                 if(app != null)
                 {
-                    ret.AddRange(app);
-                    return ret.ToArray();
+                    ret.AddRange(app);                    
                 }
                 
             }
