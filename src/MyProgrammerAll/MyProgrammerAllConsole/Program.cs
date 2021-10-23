9999d32ece76c65aa56bff71f325cd7b1e19387b
ignatandrei <ignatandrei@yahoo.com>
2021-10-23 11:13:02 +0300

    test

--- src/MyProgrammerAll/MyProgrammerAllConsole/Program.cs
+++ src/MyProgrammerAll/MyProgrammerAllConsole/Program.cs
@@ -1,4 +1,8 @@
 ﻿using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MyProgrammerAll;
 using MyProgrammerBase;
 using MyProgrammerVSProjects;
@@ -12,6 +16,7 @@ using System.CommandLine.Invocation;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,7 +25,57 @@ namespace MyProgrammerAllConsole
 {
     class Program
     {
-        async static Task<int> Main(string[] args)
+        async static Task Main(string[] args)
+        {
+            //https://docs.microsoft.com/en-us/azure/azure-monitor/app/worker-service
+            // Create the DI container.
+            IServiceCollection services = new ServiceCollection();
+
+            // Being a regular console app, there is no appsettings.json or configuration providers enabled by default.
+            // Hence instrumentation key and any changes to default logging level must be specified here.
+            services.AddLogging(loggingBuilder => loggingBuilder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>("Category", LogLevel.Information));
+            services.AddApplicationInsightsTelemetryWorkerService(
+                aiso =>
+                {
+                    aiso.ConnectionString = "InstrumentationKey=4772445f-40dd-44ae-b7d5-2c2ea33b9de3;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/";
+                    aiso.EnableDebugLogger = true;
+                    aiso.AddAutoCollectedMetricExtractor = true;
+                }
+                );
+
+            // Build ServiceProvider.
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            // Obtain logger instance from DI.
+            ILogger<Program> logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+            // Obtain TelemetryClient instance from DI, for additional manual tracking or to flush.
+            var telemetryClient = serviceProvider.GetRequiredService<TelemetryClient>();
+
+            var httpClient = new HttpClient();
+            var i = 0;
+            while (i<2) // This app runs indefinitely. replace with actual application termination logic.
+            {
+                i++;
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                // Replace with a name which makes sense for this operation.
+                using (telemetryClient.StartOperation<RequestTelemetry>("operation"))
+                {
+                    logger.LogWarning("A sample warning message. By default, logs with severity Warning or higher is captured by Application Insights");
+                    logger.LogInformation("Calling google.com");
+                    var res = await httpClient.GetAsync("https://google.com");
+                    logger.LogInformation("Calling google completed with status:" + res.StatusCode);
+                    telemetryClient.TrackEvent("google call event completed");
+                }
+                Console.WriteLine(i);
+                await Task.Delay(2000);
+            }
+            telemetryClient.Flush();
+            await Task.Delay(2000);
+        }
+
+        async static Task<int> Main1(string[] args)
         {
             using var tracerProvider = Sdk.CreateTracerProviderBuilder()
                .SetResourceBuilder(ResourceBuilder.CreateDefault()
