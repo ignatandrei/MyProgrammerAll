8ba16a1dd9e89bad0ffd3109e079a115f41a7e2e
ignatandrei <ignatandrei@yahoo.com>
2021-10-24 15:23:21 +0300

    no run

--- src/MyProgrammerAll/AutoCI.ps1
+++ src/MyProgrammerAll/AutoCI.ps1
@@ -1,5 +1,5 @@
 cls
 .\AutoMethod.ps1
 dotnet build --force
-dotnet run --project MyProgrammerAllConsole\MyProgrammerAllConsole.csproj -- export programsWinget
+#dotnet run --project MyProgrammerAllConsole\MyProgrammerAllConsole.csproj -- export programsWinget
 #dotnet run --project MyProgrammerAllConsole\MyProgrammerAllConsole.csproj -- export VS2019Sln
